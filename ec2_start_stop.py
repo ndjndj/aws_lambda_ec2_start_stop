03ade5f1d7c89640ee446f36c58163b5a56e40d8
ndjndj <teq.ndj@gmail.com>
2024-06-05 19:21:49 +0900

    code

--- ec2_start_stop.py
+++ ec2_start_stop.py
@@ -9,6 +9,9 @@ region = "ap-northeast-1"
 instances = [os.getenv("INSTANCE_ID")]
 ec2 = boto3.client("ec2", region_name=region)
 
+UNDEFINED_STATUS = {"Code": -1, "Name": "Undefined"}
+
+# EC2 status 
 #  0: pending
 # 16: running
 # 32: shutting-down
@@ -23,15 +26,15 @@ def desc_status_ec2():
     ec2_status = ec2_instance \
         .get("Reservations", [{}])[0] \
         .get("Instances", [{}])[0] \
-        .get("State", {"Code": 0, "Name": "Undefined"})
-    print(ec2_instance)
+        .get("State", UNDEFINED_STATUS)
+    
     return ec2_status 
 
 def lambda_handler(event, context):
     status_code = 200
     body = {}
     try:
-        query = event.get("queryStringParameters", {})
+        query = event.get("queryStringParameters", {}) 
         if query == None:
             query = {}
         action = query.get("action", "check")
@@ -39,25 +42,34 @@ def lambda_handler(event, context):
         if action == "check":
             ec2_status = desc_status_ec2()
             body = ec2_status
-        elif action == "activate":
+        elif action == "start":
             ec2_status = ec2.start_instances(InstanceIds=instances)
             body = ec2_status \
                 .get("StartingInstances", [{}])[0] \
-                .get("CurrentState", {"Code": 0, "Name": "Undefined"})
-            
-        elif action == "deactivate":
+                .get("CurrentState", UNDEFINED_STATUS)
+        elif action == "stop":
             ec2_status = ec2.stop_instances(InstanceIds=instances)
             body = ec2_status \
                 .get("StoppingInstances", [{}])[0] \
-                .get("CurrentState", {"Code": 0, "Name": "Undefined"})
+                .get("CurrentState", UNDEFINED_STATUS)
         
     except Exception as e:
         exc_type, _, exc_tb = sys.exc_info()
         print("internal error %s %s: %s" % (exc_tb.tb_lineno, exc_type, e))
         status_code = 500
-        body = {'message': 'Internal Server Error', 'details': "internal error %s %s: %s" % (exc_tb.tb_lineno, exc_type, e)}
+        body = {
+            'message': 'Internal Server Error', 
+            'details': "internal error %s %s: %s" % (exc_tb.tb_lineno, exc_type, e)
+        }
         
     return {
         'statusCode': status_code,
-        'body': json.dumps(body, ensure_ascii=False)
+        'body': json.dumps(body, ensure_ascii=False, default=defined_handler)
     }
+
+def defined_handler(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    if isinstance(obj, Decimal):
+        return float(obj)
+    return obj
